0462c772f081742c20a2632847b52f27ed0b136d
amirmalekian <amirmalekian.dev@gmail.com>
2026-05-22 16:40:17 +0330

    replace all net.http request handlers with echo handler

--- delivery/httpserver/user_server.go
+++ delivery/httpserver/user_server.go
@@ -7,17 +7,45 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (s Server) userRegister(c echo.Context) error {
-
-	var uReq userservice.RegisterRequest
-	if err := c.Bind(&uReq); err != nil {
+func (s Server) userLogin(c echo.Context) error {
+	var req userservice.LoginRequest
+	if err := c.Bind(&req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
-	resp, err := s.userSvc.Register(uReq)
+	resp, err := s.userSvc.Login(req)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, resp)
+}
+
+func (s Server) userRegister(c echo.Context) error {
+	var req userservice.RegisterRequest
+	if err := c.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	resp, err := s.userSvc.Register(req)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	return c.JSON(http.StatusCreated, resp)
+}
+
+func (s Server) userProfile(c echo.Context) error {
+	authToken := c.Request().Header.Get("Authorization")
+	claims, err := s.authSvc.ParseToken(authToken)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+	}
+
+	resp, err := s.userSvc.Profile(userservice.ProfileRequest{UserID: claims.UserID})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, resp)
 }
--- service/userservice/service.go
+++ service/userservice/service.go
@@ -35,14 +35,14 @@ type RegisterRequest struct {
 	Password    string `json:"password"`
 }
 
-type RegisterResponseUser struct {
+type UserInfo struct {
 	ID          uint   `json:"id"`
 	PhoneNumber string `json:"phone_number"`
 	Name        string `json:"name"`
 }
 
 type RegisterResponse struct {
-	User RegisterResponseUser `json:"user"`
+	User UserInfo `json:"user"`
 }
 
 func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
@@ -91,7 +91,7 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 		return RegisterResponse{}, fmt.Errorf("unexpected error, Register user: %w", err)
 	}
 
-	return RegisterResponse{RegisterResponseUser{
+	return RegisterResponse{UserInfo{
 		ID:          createdUser.ID,
 		PhoneNumber: createdUser.PhoneNumber,
 		Name:        createdUser.Name,
@@ -103,9 +103,14 @@ type LoginRequest struct {
 	Password    string `json:"password"`
 }
 
-type LoginResponse struct {
+type Tokens struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
+}
+
+type LoginResponse struct {
+	User   UserInfo `json:"user"`
+	Tokens Tokens   `json:"tokens"`
 }
 
 func (s Service) Login(req LoginRequest) (LoginResponse, error) {
@@ -129,7 +134,16 @@ func (s Service) Login(req LoginRequest) (LoginResponse, error) {
 		return LoginResponse{}, fmt.Errorf("unexpected error, refreshToken: %w", err)
 	}
 
-	return LoginResponse{AccessToken: accessToken, RefreshToken: refreshToken}, nil
+	return LoginResponse{
+		User: UserInfo{
+			ID:          user.ID,
+			PhoneNumber: user.PhoneNumber,
+			Name:        user.Name,
+		},
+		Tokens: Tokens{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken},
+	}, nil
 }
 
 func hashPassword(password string) (string, error) {
